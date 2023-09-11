729e8ccde6fd8a158a91b5bfafc9758807aa302a
shibin <shibinashraf36@gmail.com>
2023-09-11 16:10:17 +0530

    meteors added

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,19 @@ import Image from "next/image";
 
 import { BsSunFill, BsFillMoonStarsFill } from "react-icons/bs";
 import { BsFillChatQuoteFill } from "react-icons/bs";
+import { BsLinkedin, BsGithub, BsInstagram, BsGoogle } from "react-icons/bs";
 import {
-  SlSocialLinkedin,
-  SlSocialInstagram,
-  SlSocialGoogle,
-  SlSocialGithub,
-} from "react-icons/sl";
+  motion,
+  AnimatePresence,
+  useScroll,
+  useSpring,
+  useInView,
+  useTransform,
+} from "framer-motion";
 import { FiExternalLink } from "react-icons/fi";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useTheme, useSetTheme } from "./theme.store";
+import { Meteors } from "./meteor";
 
 export default function Home() {
   const theme = useTheme();
@@ -30,9 +34,12 @@ export default function Home() {
     }
   }, [theme]);
   const setTheme = useSetTheme();
+
   return (
     <>
       <div className=" flex flex-col bg-white museo dark:bg-black  dark:text-white text-black">
+        {theme === "dark" ? <Meteors number={10} /> : ""}
+
         <nav className="md:px-12 md:h-[7rem] h-[5rem] w-screen flex justify-between items-center p-8 	">
           <Image
             className="hover:grayscale	hover:scale-110 t-slomo"
@@ -41,9 +48,11 @@ export default function Home() {
             width={150}
           />
           <div className=" flex gap-8">
-            <button className="md:block hidden dark:bg-white hover:rounded-3xl t-slomo p-4 px-10  bg-black text-white dark:text-black">
-              Let's Talk
-            </button>
+            <a href="#contact">
+              <button className="md:block hidden dark:bg-white hover:rounded-3xl t-slomo p-4 px-10  bg-black text-white dark:text-black">
+                Let's Talk
+              </button>
+            </a>
             <div>
               <button onClick={setTheme}>
                 <div className=" w-12 h-12 p-2 rounded-full  dark:text-white hover:scale-150 t-slomo text-black t-white flex flex-wrap justify-center items-center">
@@ -67,46 +76,60 @@ export default function Home() {
               experiences.
             </p>
           </div>
+
           <div className="relative md:w-2/5 p-6 w-full	md:h-[20rem] h-[10rem] box-style text-center font-thin md:text-4xl opacity-90 bg-white border-[2px] dark:border-slate-500	 border-blue-200  dark:bg-black flex justify-center items-center ">
-            <div className="p-2 absolute -top-2 -right-3 rounded-full flex justify-center items-center md:text-5xl text-3xl">
+            <div className="p-2 absolute text-blue-200 -top-2 -right-3 rounded-full flex justify-center items-center md:text-5xl text-3xl">
               <BsFillChatQuoteFill />
             </div>
             Developers are the architects of the digital world, crafting
             solutions to shape the future, one line of code at a time.
           </div>
         </div>
-
         <div className="md:mt-8 mt-6 flex justify-center items-center">
           <Image src="build.svg" width={700} height={100} />
         </div>
         <div className="flex flex-col">
-          <h1 className=" text-4xl  text-center my-12 font-bold">Services</h1>
-          <div className="grid md:grid-cols-3 grid-cols-2 gap-10 mx-auto p-6 text-sm text-center">
-            <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem] border-2 ">
-              Full Stack Works
-            </div>
-            <div className=" p-2 px-4 md:py-[2rem]  md:px-[4rem] border-2">
-              Portfolio Works
-            </div>
-            <div className=" p-2 px-4 md:py-[2rem]  md:px-[4rem] border-2">
-              API Integration
-            </div>
-            <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  border-2">
-              UI/UX Design
-            </div>
-            <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  border-2">
-              Tech Support
-            </div>
-            <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  border-2">
-              Testing and Debugging
-            </div>
-          </div>
+          <AnimatePresence>
+            <motion.div
+              className=" mx-auto"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 1,
+                ease: "easeInOut",
+              }}
+            >
+              <h1 className=" text-4xl  text-center my-12 font-bold">
+                Services
+              </h1>
+              <div className="grid md:grid-cols-3 grid-cols-2 gap-10 mx-auto p-6 text-sm text-center">
+                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                  Full Stack Works
+                </div>
+                <div className=" p-2 px-4 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                  Portfolio Works
+                </div>
+                <div className=" p-2 px-4 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                  API Integration
+                </div>
+                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                  UI/UX Design
+                </div>
+                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                  Technical Support
+                </div>
+                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                  Testing and Debugging
+                </div>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
         <div className="flex flex-col">
           <h1 className=" text-4xl  text-center my-12 font-bold">Projects</h1>
           <div className="grid md:grid-cols-3 grid-cols-1 p-8 md:gap-x-[6rem] md:gap-y-[4rem] gap-[2rem] mx-auto">
             <div className="  hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	 dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 box-style  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 hover:scale-105 t-slomo flex justify-center items-center text-black  ">
                 <FiExternalLink />
               </div>
               <Image
@@ -120,11 +143,11 @@ export default function Home() {
             </div>
 
             <div className="  text-center hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500 dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 box-style  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
                 <FiExternalLink />
               </div>
               <Image
-                className="rounded-full h-[6rem] w-[7rem] mb-4"
+                className="rounded-full h-[5rem] w-[6rem] mb-4"
                 src="/peas.png"
                 width={100}
                 height={100}
@@ -136,11 +159,11 @@ export default function Home() {
             </div>
 
             <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 box-style  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
                 <FiExternalLink />
               </div>
               <Image
-                className="rounded-full h-[6rem] w-[6rem] mb-4"
+                className="rounded-full h-[5rem] w-[5rem] mb-4"
                 src="/pddrs.png"
                 width={100}
                 height={100}
@@ -152,11 +175,11 @@ export default function Home() {
             </div>
 
             <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 box-style  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
                 <FiExternalLink />
               </div>
               <Image
-                className="rounded-full h-[6rem] w-[6rem] mb-4"
+                className="rounded-full h-[5rem] w-[5rem] mb-4"
                 src="/pdfx.png"
                 width={100}
                 height={100}
@@ -168,12 +191,12 @@ export default function Home() {
             </div>
 
             <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
-              <div className="h-12 w-12  absolute   -top-2 -right-2 rounded-full bg-blue-200 box-style  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+              <div className="h-12 w-12  absolute   -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
                 <FiExternalLink />
               </div>
 
               <Image
-                className="rounded-full h-[6rem] w-[6rem] mb-4"
+                className="rounded-full h-[5rem] w-[5rem] mb-4"
                 src="/bingewatch.png"
                 width={100}
                 height={100}
@@ -186,33 +209,107 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col">
-          <h1 className=" text-4xl  text-center my-12 font-bold">Contact</h1>
+          <AnimatePresence>
+            <motion.div
+              className=" mx-auto"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 1,
+                ease: "easeInOut",
+              }}
+            >
+              <h1 className=" text-4xl  text-center  mt-20 mb-10 font-bold">
+                Certifications and Achievements
+              </h1>
+
+              <ul className="text-left md:text-xl space-y-[7px]">
+                <li>
+                  &rarr;&nbsp;Completed
+                  <span className="font-bold text-blue-400 dark:text-blue-200">
+                    &nbsp;GoogleCloudReady Facilitator program.
+                  </span>
+                </li>
+                <li>
+                  &rarr;&nbsp; Completed{" "}
+                  <span className="font-bold text-blue-400 dark:text-blue-200">
+                    IBM SkillsBuild Internship
+                  </span>{" "}
+                  for Emerging Technologies in Data Analytics (DA)
+                </li>
+                <li>
+                  &rarr;&nbsp;Completed agile virtual experience program by
+                  cognizant.
+                </li>
+                <li>
+                  &rarr;&nbsp; Completed{" "}
+                  <span className="font-bold text-blue-400 dark:text-blue-200">
+                    Cybersecurity Engineering Virtual Internship Program by SAP
+                    - Forage.
+                  </span>
+                </li>
+                <li>
+                  &rarr;&nbsp; Completed Internship Programme on artifical
+                  intelligence organized by{" "}
+                  <span className="font-bold text-blue-400 dark:text-blue-200">
+                    PaceLab, Kochi.
+                  </span>
+                </li>
+                <li>
+                  &rarr;&nbsp; Completed various courses offered by{" "}
+                  <span className="font-bold  text-blue-400 dark:text-blue-200">
+                    Coursera , EdX, Udemy on Linux, Python and latest
+                    technologies.
+                  </span>
+                </li>
+                <li>
+                  &rarr;&nbsp; Done various activities as student member of
+                  <span className="font-bold text-blue-400 dark:text-blue-200">
+                    &nbsp;Institution's Innovation Council (IIC).
+                  </span>
+                </li>
+              </ul>
+            </motion.div>
+          </AnimatePresence>
+        </div>
+        <div className="flex flex-col">
+          <h1 id="contact" className=" text-4xl  text-center mt-20 font-bold">
+            Contact
+          </h1>
+          <p className=" flex justify-center md:p-8 p-4 font-thin md:text-lg text-sm text-center">
+            If you're interested in collaborating or have any questions, feel
+            free to get in touch. I'd love to hear from you!
+          </p>
+
           <div className="flex flex-wrap gap-2 md:gap-[6rem] p-4 justify-center">
-            <a href="https://www.linkedin.com/in/shibin-ashraf/">
-              <div className="h-12 w-12  hover:scale-110  t-slomo rounded-full  flex items-center justify-center">
-                <SlSocialLinkedin />
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/shibin-ashraf/"
+            >
+              <div className="p-4 drop-shadow-md	text-xl bg-blue-200 dark:bg-white text-blue-800 hover:scale-110  t-slomo rounded-full  flex items-center justify-center">
+                <BsLinkedin />
               </div>
             </a>
-            <a href="https://github.com/shibinashraf">
-              <div className="h-12 w-12  hover:scale-110  t-slomo  rounded-full   flex items-center justify-center">
-                <SlSocialGithub />
+            <a target="_blank" href="https://github.com/shibinashraf">
+              <div className="p-4 drop-shadow-md	text-xl bg-blue-200 dark:bg-white text-red-800  hover:scale-110  t-slomo  rounded-full   flex items-center justify-center">
+                <BsGithub />
               </div>
             </a>
 
-            <a href="mailto: shibinashraf36@gmail.com">
-              <div className="h-12 w-12  hover:scale-110 t-slomo  rounded-full  flex items-center justify-center">
-                <SlSocialGoogle />
+            <a target="_blank" href="mailto: shibinashraf36@gmail.com">
+              <div className="p-4 drop-shadow-md	text-xl bg-blue-200  dark:bg-white  text-green-800  hover:scale-110 t-slomo  rounded-full  flex items-center justify-center">
+                <BsGoogle />
               </div>
             </a>
-            <a href="https://www.instagram.com/shib1n/">
-              <div className="h-12 w-12 hover:scale-110 t-slomo  rounded-full flex items-center justify-center">
-                <SlSocialInstagram />
+            <a target="_blank" href="https://www.instagram.com/shib1n/">
+              <div className="p-4 drop-shadow-md	text-xl bg-blue-200  dark:bg-white text-purple-800  hover:scale-110 t-slomo  rounded-full flex items-center justify-center">
+                <BsInstagram />
               </div>
             </a>
           </div>
         </div>
-        <footer className=" flex flex-col items-center justify-center gap-4 p-4">
-          <div className="p-2 px-6 bg-black rounded-md text-white  hover:scale-105 t-slomo dark:bg-white dark:text-black  flex justify-center  items-center">
+        <footer className="mt-8 flex flex-col items-center justify-center gap-4 p-4">
+          <div className="p-2  px-4 bg-black rounded-md text-white text-sm   hover:scale-105 t-slomo dark:bg-white dark:text-black  flex justify-center  items-center">
             <a href="#top">
               Back to top <span>&nbsp;&#8593;</span>{" "}
             </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
   darkMode: ["class", '[data-mode="dark"]'],
   theme: {
     extend: {
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+          "70%": { opacity: 1 },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: 0,
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
