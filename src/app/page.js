a56bed33ed56e5f3d88fe42e0155106f34142e5b
shibin <shibinashraf36@gmail.com>
2023-09-14 10:21:45 +0530

    padding added to certificates section and quotes random fetch script added , font changed

--- src/app/page.js
+++ src/app/page.js
@@ -13,9 +13,8 @@ import {
   useTransform,
 } from "framer-motion";
 import { FiExternalLink } from "react-icons/fi";
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useTheme, useSetTheme } from "./theme.store";
-import { Meteors } from "./meteor";
 
 export default function Home() {
   const theme = useTheme();
@@ -34,14 +33,37 @@ export default function Home() {
     }
   }, [theme]);
   const setTheme = useSetTheme();
+  const codingQuotes = [
+    "The only limit to our realization of tomorrow will be our doubts of today.",
+    "The best way to predict the future is to create it.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "Believe you can and you're halfway there.",
+    "Your time is limited, don't waste it living someone else's life.",
+    "The future belongs to those who believe in the beauty of their dreams.",
+    "Success usually comes to those who are too busy to be looking for it.",
+    "In the middle of every difficulty lies opportunity.",
+    "The best revenge is massive success.",
+    "Don't count the days, make the days count.",
+    "The road to success and the road to failure are almost exactly the same.",
+    "The way to get started is to quit talking and begin doing.",
+    "Your attitude determines your direction.",
+    "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+    "The harder you work for something, the greater you'll feel when you achieve it.",
+    "Success is walking from failure to failure with no loss of enthusiasm.",
+    "You miss 100% of the shots you don't take.",
+    "Dream big and dare to fail.",
+    "The only thing that stands between you and your dream is the will to try and the belief that it is actually possible.",
+    "Don't let yesterday take up too much of today.",
+  ];
+  const [quote, setQuote] = useState("");
 
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * codingQuotes.length);
+    setQuote(codingQuotes[randomIndex]);
+  }, []);
   return (
     <>
-      <div className="  flex flex-col bg-white museo dark:bg-black  dark:text-white text-black ">
-        <div className="md:block hidden overflow-hidden max-w-full">
-          {theme === "dark" ? <Meteors number={10} /> : ""}
-        </div>
-
+      <div className="  flex flex-col bg-white mont dark:bg-black  dark:text-white text-black ">
         <nav className="md:px-12 md:h-[7rem] h-[5rem] w-screen flex justify-between items-center p-8 	">
           <Image
             className="hover:grayscale	hover:scale-110 t-slomo"
@@ -71,7 +93,9 @@ export default function Home() {
             </div>
             <p className="text-lg">
               Hi, i am
-              <span className="text-blue-200 text-center text-2xl">Shibin</span>
+              <span className="text-blue-200 museo text-center text-2xl">
+                &nbsp;Shibin
+              </span>
               , a full-stack developer and UI/UX designer from India, deeply
               passionate about React.js, product design, and creating innovative
               software. I thrive on turning ideas into seamless digital
@@ -79,18 +103,18 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="relative md:w-2/5 p-6 w-full	md:h-[20rem] h-[10rem] box-style text-center font-thin md:text-4xl opacity-90 bg-white border-[2px] dark:border-slate-500	 border-blue-200  dark:bg-black flex justify-center items-center ">
+          <div className="relative md:w-2/5 p-6 w-full museo font-medium 	md:h-[20rem] h-[10rem] box-style text-center opacity-80 md:text-4xl bg-white border-[2px] dark:border-slate-500	 border-blue-200  dark:bg-black flex justify-center items-center ">
             <div className="p-2 absolute text-blue-200 -top-2 -right-3 rounded-full flex justify-center items-center md:text-5xl text-3xl">
               <BsFillChatQuoteFill />
             </div>
-            Developers are the architects of the digital world, crafting
-            solutions to shape the future, one line of code at a time.
+            {quote}
           </div>
         </div>
         <div className="md:mt-8 mt-6 flex justify-center items-center">
           <Image src="build.svg" width={700} height={100} />
         </div>
         <div className="flex flex-col">
+          <h1 className=" text-4xl  text-center my-12 font-bold">Services</h1>
           <AnimatePresence>
             <motion.div
               className=" mx-auto"
@@ -101,26 +125,23 @@ export default function Home() {
                 ease: "easeInOut",
               }}
             >
-              <h1 className=" text-4xl  text-center my-12 font-bold">
-                Services
-              </h1>
               <div className="grid md:grid-cols-3 grid-cols-2 gap-10 mx-auto p-6 text-sm text-center">
-                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                <div className="p-4 px-6 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-300  hover:scale-105 t-slomo">
                   Full Stack Works
                 </div>
-                <div className=" p-2 px-4 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                <div className=" p-4 px-6 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-300  hover:scale-105 t-slomo">
                   Portfolio Works
                 </div>
-                <div className=" p-2 px-4 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                <div className=" p-4 px-6 md:py-[2rem]  md:px-[4rem] shadow-md rounded-xl border-2 border-slate-300  hover:scale-105 t-slomo">
                   API Integration
                 </div>
-                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                <div className="p-4 px-6 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-300  hover:scale-105 t-slomo">
                   UI/UX Design
                 </div>
-                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                <div className="flex items-center justify-center p-4 px-6 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-300  hover:scale-105 t-slomo">
                   Technical Support
                 </div>
-                <div className="p-2 px-4 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-500  hover:scale-105 t-slomo">
+                <div className="p-4 px-6 md:py-[2rem]  md:px-[4rem]  shadow-md rounded-xl border-2 border-slate-300  hover:scale-105 t-slomo">
                   Testing and Debugging
                 </div>
               </div>
@@ -129,87 +150,100 @@ export default function Home() {
         </div>
         <div className="flex flex-col">
           <h1 className=" text-4xl  text-center my-12 font-bold">Projects</h1>
-          <div className="grid md:grid-cols-3 grid-cols-1 p-8 md:gap-x-[6rem] md:gap-y-[4rem] gap-[2rem] mx-auto">
-            <div className="  hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	 dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 hover:scale-105 t-slomo flex justify-center items-center text-black  ">
-                <FiExternalLink />
-              </div>
-              <Image
-                className="rounded-full h-[6rem] w-[6rem] mb-4"
-                src="/udyo.png"
-                width={100}
-                height={100}
-              />
 
-              <div className="text-lg font-bold">Udyo! - Jobs Portal</div>
-            </div>
+          <AnimatePresence>
+            <motion.div
+              className=" mx-auto"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 1,
+                ease: "easeInOut",
+              }}
+              className="grid md:grid-cols-3 grid-cols-1 p-8 md:gap-x-[6rem] md:gap-y-[4rem] gap-[2rem] mx-auto"
+            >
+              <div className="  hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	 dark:bg-black">
+                <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200 hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+                  <FiExternalLink />
+                </div>
+                <Image
+                  className="rounded-full h-[6rem] w-[6rem] mb-4"
+                  src="/udyo.png"
+                  width={100}
+                  height={100}
+                />
 
-            <div className="  text-center hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500 dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
-                <FiExternalLink />
-              </div>
-              <Image
-                className="rounded-full h-[5rem] w-[6rem] mb-4"
-                src="/peas.png"
-                width={100}
-                height={100}
-              />
-
-              <div className="text-lg font-bold">
-                PEAS - Stress Management Companion
-              </div>
-            </div>
-
-            <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
-                <FiExternalLink />
-              </div>
-              <Image
-                className="rounded-full h-[5rem] w-[5rem] mb-4"
-                src="/pddrs.png"
-                width={100}
-                height={100}
-              />
-
-              <div className="text-lg text-center font-bold">
-                PDDRS - Machine Learning
-              </div>
-            </div>
-
-            <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
-              <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
-                <FiExternalLink />
-              </div>
-              <Image
-                className="rounded-full h-[5rem] w-[5rem] mb-4"
-                src="/pdfx.png"
-                width={100}
-                height={100}
-              />
-
-              <div className="text-lg text-center font-bold">
-                pdfX - PDF Toolkit
-              </div>
-            </div>
-
-            <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
-              <div className="h-12 w-12  absolute   -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
-                <FiExternalLink />
+                <div className="text-lg font-regular">Udyo! - Jobs Portal</div>
               </div>
 
-              <Image
-                className="rounded-full h-[5rem] w-[5rem] mb-4"
-                src="/bingewatch.png"
-                width={100}
-                height={100}
-              />
+              <div className="  text-center hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500 dark:bg-black">
+                <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+                  <FiExternalLink />
+                </div>
+                <Image
+                  className="rounded-full h-[5rem] w-[6rem] mb-4"
+                  src="/peas.png"
+                  width={100}
+                  height={100}
+                />
 
-              <div className="text-lg text-center font-bold">
-                Binge Watch - Movies
+                <div className="text-lg font-regular">
+                  PEAS - Stress Management Companion
+                </div>
               </div>
-            </div>
-          </div>
+
+              <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
+                <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+                  <FiExternalLink />
+                </div>
+                <Image
+                  className="rounded-full h-[5rem] w-[5rem] mb-4"
+                  src="/pddrs.png"
+                  width={100}
+                  height={100}
+                />
+
+                <div className="text-lg text-center font-regular">
+                  PDDRS - Machine Learning
+                </div>
+              </div>
+
+              <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
+                <div className="h-12 w-12 absolute -top-2 -right-2 rounded-full bg-blue-200  hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+                  <FiExternalLink />
+                </div>
+                <Image
+                  className="rounded-full h-[5rem] w-[5rem] mb-4"
+                  src="/pdfx.png"
+                  width={100}
+                  height={100}
+                />
+
+                <div className="text-lg text-center font-regular">
+                  pdfX - PDF Toolkit
+                </div>
+              </div>
+
+              <div className=" hover:scale-105 t-slomo p-6 relative flex flex-col items-center justify-center  h-auto w-[15rem] box-style bg-white border-[2px] dark:border-slate-500	  dark:bg-black">
+                <div className="h-12 w-12  absolute   -top-2 -right-2 rounded-full bg-blue-200   hover:scale-105 t-slomo flex justify-center items-center text-black  ">
+                  <FiExternalLink />
+                </div>
+
+                <Image
+                  className="rounded-full h-[5rem] w-[5rem] mb-4"
+                  src="/bingewatch.png"
+                  width={100}
+                  height={100}
+                />
+
+                <div className="text-lg text-center font-regular">
+                  Binge Watch - Movies
+                </div>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
+
         <div className="flex flex-col p-8">
           <AnimatePresence>
             <motion.div
